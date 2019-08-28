864d8f1aa91766efeff1b7dfbb6a1532e8b9d7b6
Smith <smith.mugalu@andela.com>
2019-08-28 15:35:22 +0300

    Initial Commit

---
+++ Stormy/CurrentWeather.swift
@@ -0,0 +1,18 @@
+//
+//  CurrentWeather.swift
+//  Stormy
+//
+//  Created by Smithers on 28/08/2019.
+//  Copyright © 2019 Treehouse. All rights reserved.
+//
+
+import Foundation
+
+struct CurrentWeather {
+    
+    let temperature : Double
+    let humidity : Double
+    let precipProbability : Double
+    let summary : String
+    let icon : String
+}
